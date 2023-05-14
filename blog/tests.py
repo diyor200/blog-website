0a5309421cd2e9283c2b56b7f69f1199d132097e
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2023-05-14 14:44:02 +0500

    Created comment system

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostListTest(TestCase):
+    def setUp(self):
+        self.author = User.objects.create(username="testuser")
+        self.post = Post.objects.create(title="Test Post",
+                                        slug="test-post",
+                                        author=self.author,
+                                        body="This is a test post",
+                                        status=Post.Status.PUBLISHED)
+
+    def test_post_list_view(self):
+        url = reverse('blog:post_list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.post.title)
+
+    def test_post_detail_view(self):
+        url = reverse('blog:post_detail', args=[self.post.publish.year, self.post.publish.month, self.post.publish.day, self.post.slug])
+        response = self.client.get(url)
+        print(url)
+
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(url, self.post.get_absolute_url())
+
+    def test_post_comment_view(self):
+        url = reverse('blog:post_comment', args=[self.post.id])
+        response = self.client.get(url)
+        print(url)
+        print(response)
+
+        # self.assertEqual(response.status_code, 200)
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dabdulaxatov036@gmail.com'
+EMAIL_HOST_PASSWORD = 'clvkfijykijxluoo'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
